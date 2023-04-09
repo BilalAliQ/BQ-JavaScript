f085d8a7ca3463364ed6ef3fb537d3a196e6a98b
Bilal <Bilal3101998@gmail.com>
2023-04-09 10:33:44 +0500

    FEAT:JS 1 to 9 Chap

---
+++ scripts/app.js
@@ -0,0 +1,42 @@
+// DAY 1 - 9 APR 
+
+alert("Hello Zohaib")
+document.write("<u>Zohaib</u>")
+
+prompt("Username Please")
+
+var banoQabil //camelCase
+var BanoQabil //PascalCase
+var bano_qabil //snake_case
+
+var username = prompt("Username Please")
+var HaalChal = "Kiya Haal hai?"
+console.log("Hello " + username + " " + HaalChal)
+
+var age = 19
+console.log(typeof age)
+console.log(typeof HaalChal)
+
+
+
+
+var age = +prompt("Your Age Please?")
+var fage = +prompt("Your Father Age Please?")
+// console.log(Number(fage)+Number(age))
+console.log(age+fage)
+
+console.log(19+2)
+console.log(19-2)
+console.log(19*2)
+console.log(19/2)
+console.log(2**3)
+
+var num = 1
+console.log(num)
+// num = num+1
+// num +=1
+console.log(++num)
+console.log(num)
+ console.log(((1+8)*(5/8))+((7-34)/(8+9)))
+
+
